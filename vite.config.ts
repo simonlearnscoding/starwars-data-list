c537d8a1ca63f523e2ab47dbe0ac739bae946247
simonlearnscoding <simon.muscas@gmail.com>
2025-03-05 14:32:39 +0100

    set root as base

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,13 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-const basenameProd = '/react-shadcn-starter';
-
 export default defineConfig(({ command }) => {
-  const isProd = command === 'build';
-
   return {
-    base: isProd ? basenameProd : '/',
+    base: '/',
     plugins: [react()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
-      },
-    },
-    define: {
-      global: {
-        basename: isProd ? basenameProd : '',
       },
     },
   };
