803d0c6e55704f3465a86d171a47c49e77eea17c
simonlearnscoding <simon.muscas@gmail.com>
2025-03-05 13:10:51 +0100

    set store import as global import

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
-import store from './store/store';
+import store from '@/store/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
